4bc179f239913fb9852caa619d8bdb25b56ba046
sergey <sergey>
2018-04-25 14:45:01 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class App extends Component {
+
+  addTrack(){
+    console.log('addTrack');
+  }
+
   render() {
     console.log(this.props.testStore);
     return (
       <div>
         <input type="text" />
-        <button>Add track</button>
+        <button onClick={ this.addTrack.bind(this) }>Add track</button>
         <ul>
           {this.props.testStore.map((track, index) =>
             <li key={index}>{track}</li>
